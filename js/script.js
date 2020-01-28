ae8493633c13d8c1370e71e4638ef584f25a751e
Tiffany Donner <tiffany.donner@stsupport.se>
2020-01-28 08:31:46 +0000

    Challenge Solution Traversing Up and Down the DOM Tree

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
+//wait until page is ready
 $(document).ready(function() {
-    // put your code here
+	//sets <a> element within paragraph  to yellow  
+	$("p").click(function(){
+		$(this).children("a").css("background-color", "yellow"); /* returns all the <a> child elements that are 
+		within this paragraph*/
+	});
 });
