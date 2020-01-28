4852dda1ab0399207ae3dcb7bf1dc9114196febf
Tiffany Donner <tiffany.donner@stsupport.se>
2020-01-28 09:22:41 +0000

    Button clicked function working correctly .prev added

--- js/script.js
+++ js/script.js
@@ -4,10 +4,15 @@ $(document).ready(function() {
 	$("p").click(function(){
 		$(this).children("a").css("background-color", "yellow"); /* returns all the <a> child elements that are 
 		within this paragraph*/
-    });
-    
-    // Toggle the visibility of the paragraph when a button is clicked 
+	});
+
+	// Toggle the visibility of the paragraph when a button is clicked 
 	$("button").click(function(){
-		$(this).next().slideToggle('slow');
+		$(this).prev().slideToggle('slow');
+	});
+
+	// Open the paragraph once the image is clicked
+	$("img").click(function() {
+		$(this).next().children("p").slideDown();
 	});
 });
